0fc0e2d3caeb36a65d7b34403074c8390181fac7
JuanRodriguez <juandavid.03211@gmail.com>
2025-04-05 11:45:40 -0500

    Title Modified Second Time

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Este es el tìtulo uno modificado</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
